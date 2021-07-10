f6438edf851bd3251816385f4d021917eef3527d
guozhuang <Guo15210875897>
2021-07-10 11:10:34 +0800

    templates

---
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+
+//模版引擎测试
+@Controller
+@RequestMapping
+public class ThymeleafController {
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        // 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+        view.setViewName("index");
+        // 设置属性
+        view.addObject("title", "我的第一个WEB页面");
+        view.addObject("desc", "欢迎进入web 系统");
+        Author author = new Author();
+        author.setAge(31);
+        author.setEmail("xxxx");
+        author.setName("gz");
+        view.addObject("author", author);
+        return view;
+    }
+
+    //暂时将结构体放在这里
+    class Author {
+        private int age;
+        private String name;
+        private String email;
+        // 省略 get set
+
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+}
