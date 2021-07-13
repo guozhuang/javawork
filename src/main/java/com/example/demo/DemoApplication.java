a75affce3ed01da4ce433268061b24af729eb05a
guozhuang <Guo15210875897>
2021-07-13 18:18:59 +0800

    redis todo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Profile;
 
+//此处注解非常必要
 @SpringBootApplication
-
 @MapperScan(value = "com.example.demo.mapper")
+//@Profile()//基于环境切换的配置
 public class DemoApplication {
 
     public static void main(String[] args) {
